69db10858b9d86ead3bd7dfb1db655e06d27fe90
amanofuyu <amanofuyu@nai.ne>
2025-01-09 15:43:40 +0800

    打包配置

--- rslib.config.ts
+++ rslib.config.ts
@@ -4,8 +4,39 @@ export default defineConfig({
   lib: [
     {
       format: 'esm',
-      syntax: 'esnext',
-      dts: true,
+      autoExtension: false,
+      output: {
+        filename: {
+          js: '[name].mjs',
+        },
+      },
+      dts: {
+        bundle: true,
+      },
+    },
+    {
+      format: 'cjs',
+      autoExtension: false,
+      output: {
+        filename: {
+          js: '[name].cjs',
+        },
+      },
+    },
+    {
+      format: 'umd',
+      umdName: 'NQR',
+      autoExtension: false,
+      output: {
+        filename: {
+          js: '[name].js',
+        },
+        // 覆盖默认值，即可实现压缩？
+        minify: {},
+      },
     },
   ],
+  output: {
+    target: 'web',
+  },
 });
